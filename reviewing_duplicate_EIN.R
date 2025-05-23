8e3d3700ba9192e12b3d91717988ed4801f86fa7
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2025-05-23 08:51:52 -0400

    new comments and update to run faster

--- reviewing_duplicate_EIN.R
+++ reviewing_duplicate_EIN.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(data.table)
-library(cchsflow)
+library(cchsflow) # for is_equal(), which will return TRUE for NA==NA
 
 # function that takes in data_file and returns a data table with information about which
 # columns differ when a set of rows have the same EIN
@@ -14,18 +14,15 @@ duplicateEIN2_info <- function(data_file){
     core_duplicates <- data_file |>
         filter(EIN2 %in% duplicate_EIN2$EIN2)
     
-    # Instantiate list of counts; entry i is number of times there was a mismatch in the i-th column
-    count_list <- rep(0, ncol(data_file))
+    # Split the data by EIN2 to loop over groups
+    duplicate_groups <- split(core_duplicates, core_duplicates$EIN2)
     
-    # This is to count the number of times the difference might be due to missing values
+    # Initialize counters, one for counting differences and one for counting differences that may be due to missing value
+    count_list <- rep(0, ncol(data_file))
     count_list_NA <- rep(0, ncol(data_file))
     
     # For each EIN2 in list, compare the corresponding rows and note columns where there are differences
-    for (idx in 1:nrow(duplicate_EIN2)) {
-        #print(paste(idx, ",", duplicate_EIN2$EIN2[idx], ",", duplicate_EIN2$n[idx]), sep=" ")
-        ein <- duplicate_EIN2$EIN2[idx] # current EIN
-        subset <- core_duplicates |> filter(EIN2 == ein) # give me the rows corresponding to current EIN
-        
+    for (subset in duplicate_groups) {
         # depending on how many times the EIN was repeated, do different comparisons (if more than 3 times, skipped for now)
         if (nrow(subset) == 2){
             diff_cols <- is_equal(subset[1,], subset[2,])
@@ -34,7 +31,7 @@ duplicateEIN2_info <- function(data_file){
             diff_cols <- (is_equal(subset[1,], subset[2,]) & is_equal(subset[1,],subset[3,]) & is_equal(subset[2,],subset[3,]))
             diff_cols_NA <- ((subset[1,] == subset[2,]) & (subset[2,] == subset[3,]) & (subset[1,] == subset[3,]))
         } else {
-            print(paste("EIN", ein, "occurs", nrow(subset), "times, skipped", sep = " "))
+            print(paste("EIN", subset[1,1], "occurs", nrow(subset), "times, skipped", sep = " "))
             next
         }
         # indices of columns where there was a mismatch (or missing value)
@@ -47,10 +44,13 @@ duplicateEIN2_info <- function(data_file){
     }
     
     # Create a table with the info we just gathered
-    duplicates_info <- data.table(variable_name = colnames(data_file), num_differences = count_list, num_NA = count_list_NA, dtype = sapply(core_data_1989, class))
+    duplicates_info <- data.table(variable_name = colnames(data_file), num_differences = count_list, num_NA = count_list_NA, dtype = sapply(data_file, class))
     return(list(info = duplicates_info, dupes = duplicate_EIN2))
 }
 
-# Example Usage:
+# Example Usage (from another file):
+# source("reviewing_duplicate_EIN.R")
 # core_data_2022 <- read_csv("CORE/CORE-2022-501C3-CHARITIES-PZ-HRMN.csv")
 # dupe2022_info <- duplicateEIN2_info(core_data_2022)
+# dupe2022_info_tab <- dupe2022_info$info
+# dupe2022_info_EIN_list <- dupe2022_info$dupes 
